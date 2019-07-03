1f5c76c1f19bd0b6e604c881920b2da6e0f2c8d3
Todd Sharp <cfsilence@gmail.com>
2019-07-03 11:34:31 -0400

    fix native image

--- src/main/java/codes/recursive/cnms/ords/Application.java
+++ src/main/java/codes/recursive/cnms/ords/Application.java
@@ -5,6 +5,11 @@ import io.micronaut.runtime.Micronaut;
 public class Application {
 
     public static void main(String[] args) {
+        String libsunecPath = System.getenv("LIBSUNEC_PATH");
+        if( libsunecPath != null ) {
+            System.setProperty("java.library.path", libsunecPath);
+            System.loadLibrary("sunec");
+        }
         Micronaut.run(Application.class);
     }
 }
